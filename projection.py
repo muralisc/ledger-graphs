b15a60437ffab2618a2b5634aae7cbc69a8cc626
Murali S <muralisc@gmail.com>
2020-05-17 19:04:46 +0530

    Split string for ease of readability

--- projection.py
+++ projection.py
@@ -21,8 +21,32 @@ Last_age=35
 YEAR=INITIAL_CORPUS_ENDING_YEAR
 PREV_CORPUS=INITIAL_CORPUS
 year = INITIAL_CORPUS_ENDING_YEAR
-print("AGE,YEAR,Inflation,Savings,Rate,Interest,NetWorth,| SalAfterTax,SalBeforeTax,FIRE_Now")
-print("{:3},{:4}, , ,  ,       ,{:6.2f},|".format(AGE_ENDING_YEAR, year, PREV_CORPUS))
+titles = ""
+titles += "AGE"
+titles += ",YEAR"
+titles += ",Inflation"
+titles += ",Savings"
+titles += ",Rate"
+titles += ",Interest"
+titles += ",NetWorth"
+titles += ",|"
+titles += ",SalAfterTax"
+titles += ",SalBeforeTax"
+titles += ",FIRE_Now"
+print(titles)
+titles = ""
+titles += "{:5}".format(AGE_ENDING_YEAR)
+titles += ",{:5}".format(year)
+titles += ", " 
+titles += ", "
+titles += ", "
+titles += ", "
+titles += ",{:5.2f}" .format(PREV_CORPUS)
+titles += ",|"
+titles += ", "
+titles += ", "
+titles += ", "
+print(titles)
 for age in range(AGE_ENDING_YEAR+1, Last_age+1):
   INTEREST=PREV_CORPUS*RATE_OF_INTEREST/100
   NEXT_CORPUS=INTEREST + PREV_CORPUS + SAVINGS_AMOUNT_PER_YEAR
@@ -30,7 +54,18 @@ for age in range(AGE_ENDING_YEAR+1, Last_age+1):
   SALARY_REQ_AFTER_TAX=2*SAVINGS_AMOUNT_PER_YEAR
   SALARY_BEFORE_TAX = SALARY_REQ_AFTER_TAX / 0.7
   FIRE_CORPUS_REQ = SAVINGS_AMOUNT_PER_YEAR / RATE_OF_INTEREST * 100
-  print("{:3},".format(age), end="")
-  print("{:4},{}%, {:5.2f},{:3},{:5.2f},{:6.2f},| {:0.2f},{:0.2f},{:0.2f}".format( year, Inflation, SAVINGS_AMOUNT_PER_YEAR, RATE_OF_INTEREST, INTEREST, NEXT_CORPUS, SALARY_REQ_AFTER_TAX, SALARY_BEFORE_TAX, FIRE_CORPUS_REQ))
+  msg = ""
+  msg += "{:5}"     .format (age)
+  msg += ",{:5}"    .format (year)
+  msg += ",{}%"     .format (Inflation)
+  msg += ",{:5.2f}" .format (SAVINGS_AMOUNT_PER_YEAR)
+  msg += ",{:5}"    .format (RATE_OF_INTEREST)
+  msg += ",{:5.2f}" .format (INTEREST)
+  msg += ",{:5.2f}" .format (NEXT_CORPUS)
+  msg += ",|"
+  msg += ",{:5.2f}" .format (SALARY_REQ_AFTER_TAX)
+  msg += ",{:5.2f}" .format (SALARY_BEFORE_TAX)
+  msg += ",{:5.2f}" .format (FIRE_CORPUS_REQ)
+  print(msg)
   PREV_CORPUS= NEXT_CORPUS
   SAVINGS_AMOUNT_PER_YEAR *= 1.00 + Inflation/100
