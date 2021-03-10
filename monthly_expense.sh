fb8d9e317f1e25e1b22d5a9731737f19372655a7
Murali S <muralisc@gmail.com>
2021-03-10 17:08:50 +0000

    add scripts

--- monthly_expense.sh
+++ monthly_expense.sh
@@ -10,29 +10,22 @@ fi
 ledger -f $LEDGER_FILE -X INR -jMn reg --plot-amount-format="%(format_date(date, \"%Y-%m-%d\")) %(abs(quantity(scrub(floor(display_amount)))))\n" '^Income' "$@" > ledgeroutput1.tmp
 ledger -f $LEDGER_FILE -X INR -jMn reg --plot-amount-format="%(format_date(date, \"%Y-%m-%d\")) %(abs(quantity(scrub(floor(display_amount)))))\n" '^Expe' "$@" > ledgeroutput2.tmp
 
+python3 monthly_expense.py | sort > ledgeroutput3.tmp
+
 (cat <<EOF) | gnuplot
   set terminal $LEDGER_TERM
-  set style data histogram
-  set style histogram clustered gap 1
-  set style fill transparent solid 0.4 noborder
+  set xdata time
+  set timefmt "%Y-%m-%d"
   set xtics nomirror scale 0 rotate by -55
-  set ytics add ('' 0) scale 0
-  set border 1
   set grid ytics
   set title "Monthly Income and Expenses"
   set ylabel "Amount"
-  # Explanation
-  # code: "ledgeroutput1.tmp" using 2:xticlabels(strftime('%b-%y', strptime('%Y-%m-%d', strcol(1)))) title "Income" linecolor rgb "light-salmon", \
-  # doc :
-  #                  '' using 0:2:2 with labels left font "Courier,12" rotate by 45 offset -4,0.5 textcolor linestyle 0 notitle, \
-  # doc : plot lables graph with x,y at (0 column,2 column) using label same as that at column 2
-  # "ledgeroutput2.tmp" using 2 title "Expenses" linecolor rgb "light-red", \
-  #                  '' using 0:2:2 with labels left font "Courier,12" rotate by 45 offset 0,0.5 textcolor linestyle 0 notitle
+  set style line 1 lc rgb '#0060ad' lt 1 lw 2 pt 7 pi -1 ps 1.5
   plot \
-    "ledgeroutput1.tmp" using 2:xticlabels(strftime('%b-%y', strptime('%Y-%m-%d', strcol(1)))) title "Income" linecolor rgb "light-salmon", \
-                     '' using 0:2:2 with labels left font "Courier,12" rotate by 45 offset -4,0.5 textcolor linestyle 0 notitle, \
-    "ledgeroutput2.tmp" using 2:xticlabels(strftime('%b-%y', strptime('%Y-%m-%d', strcol(1)))) title "Expenses" linecolor rgb "light-red", \
-                     '' using 0:2:2 with labels left font "Courier,12" rotate by 45 offset 0,0.5 textcolor linestyle 0 notitle
+    "ledgeroutput1.tmp" using 1:2 with linespoints title "Income" linecolor rgb "blue", \
+    "ledgeroutput2.tmp" using 1:2 with linespoints title "Expense" ls 1, \
+                     '' using 1:2:2 with labels left font "Courier,12" rotate by 90 offset 1,1 textcolor linestyle 1 notitle, \
+    "ledgeroutput3.tmp" using 1:2 with linespoints title "Income - Expense" linecolor rgb "red"
 EOF
 
 #rm ledgeroutput*.tmp
