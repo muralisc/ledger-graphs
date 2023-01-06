c31aebe26c1cadf62c2035cac46d4b90bd4c207a
Murali S <muralisc@gmail.com>
2023-01-06 16:34:54 +0000

    Commit last year

--- graphs/1send_projection_to_influx.sh
+++ graphs/1send_projection_to_influx.sh
@@ -75,3 +75,4 @@ curl --request POST \
 mkdir -p ~/public_html/
 cp $PROJECTION_FOLDER/ledger_projection.png ~/public_html/ledger_projection.png
 cp $PROJECTION_FOLDER/ledger_monthly_inc_exp.png ~/public_html/ledger_monthly_inc_exp.png
+cp $PROJECTION_FOLDER/ledger_monthly_payee.png ~/public_html/ledger_monthly_payee.png
