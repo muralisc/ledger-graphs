02616622950654b5fc4fb17de25163e5a670b31e
Murali S <muralisc@gmail.com>
2023-07-12 19:17:28 +0100

    Add amex for May

--- graphs/copy_ledger_files_to_public_html.sh
+++ graphs/copy_ledger_files_to_public_html.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # COPY GENERATED PNG TO PUBLIC FOLDER
+
+PROJECTION_FOLDER=$(find /var/tmp -iname "ledger_20*" 2> /dev/null | sort | tail -1)
+
 mkdir -p ~/public_html/
 cp $PROJECTION_FOLDER/ledger_projection.png ~/public_html/ledger_projection.png
 cp $PROJECTION_FOLDER/ledger_monthly_inc_exp.png ~/public_html/ledger_monthly_inc_exp.png
