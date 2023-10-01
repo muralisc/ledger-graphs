7878dc391a41d4c87734fb78f826f052ff13880b
Murali S <muralisc@gmail.com>
2023-10-01 23:49:07 +0100

    Update ledger files

--- graphs/1plot_ledger_projection.sh
+++ graphs/1plot_ledger_projection.sh
@@ -9,7 +9,7 @@ export LEDGER_FILE=$HOME/shared_folders/minimal/Pensieve/textfiles/ledger/ledger
 export LEDGER_PRICE_DB=$HOME/shared_folders/minimal/Pensieve/textfiles/ledger/pricedb.txt
 
 ledger_run_date=$(date +%Y-%m-%d)
-FOLDER="/var/tmp/ledger_${ledger_run_date}"
+FOLDER="/var/tmp/ledger_1_${ledger_run_date}"
 mkdir -p $FOLDER
 
 if [[ -z "$LEDGER_TERM" ]]; then
