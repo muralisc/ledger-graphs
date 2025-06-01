5e3b0bc8bb2c48ea7f1dcb381c57b42be75ff7c6
muralisc <muralisc@gmail.com>
2025-06-01 11:04:09 +0100

    Add script to make example graphs

---
+++ scripts/make_example_graphs.sh
@@ -0,0 +1,2 @@
+bash ~/src/ledger-graphs/graphs/2plot_ledger_monthly_income_expense.sh ~/src/ledger-graphs/assets/example.ledger /dev/null
+bash ~/src/ledger-graphs/graphs/3plot_ledger_monthly_expense_accounts.sh ~/src/ledger-graphs/assets/example.ledger /dev/null
