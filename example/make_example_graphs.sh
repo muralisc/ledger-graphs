2a1d933b4a2bf77605dfa1f9256e4b2897445124
muralisc <muralisc@gmail.com>
2026-06-07 10:29:01 +0100

    Rename plot 2

--- example/make_example_graphs.sh
+++ example/make_example_graphs.sh
@@ -1,2 +1,2 @@
-bash ~/src/ledger-graphs/v1/2plot_ledger_monthly_income_expense.sh ~/src/ledger-graphs/assets/example.ledger /dev/null
+bash ~/src/ledger-graphs/v1/2-monthly-lastmonth-income_expense.sh ~/src/ledger-graphs/assets/example.ledger /dev/null
 bash ~/src/ledger-graphs/v1/3plot_ledger_monthly_expense_accounts.sh ~/src/ledger-graphs/assets/example.ledger /dev/null
