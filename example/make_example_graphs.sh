5c7c43529cafdc7a19479bb8e839877ceb4703ea
muralisc <muralisc@gmail.com>
2026-06-07 13:05:01 +0100

    Pass FOLDER as arg to script 2

--- example/make_example_graphs.sh
+++ example/make_example_graphs.sh
@@ -1,2 +1,4 @@
-bash ~/src/ledger-graphs/v1/2-monthly-lastmonth-income_expense.sh ~/src/ledger-graphs/assets/example.ledger /dev/null
-bash ~/src/ledger-graphs/v1/3-monthly-lastmonth-expense_accounts.sh ~/src/ledger-graphs/assets/example.ledger /dev/null
+EXAMPLE_FOLDER="/var/tmp/ledger/example"
+
+bash ~/src/ledger-graphs/v1/2-monthly-lastmonth-income_expense.sh ~/src/ledger-graphs/assets/example.ledger /dev/null "${EXAMPLE_FOLDER}/2"
+bash ~/src/ledger-graphs/v1/3-monthly-lastmonth-expense_accounts.sh ~/src/ledger-graphs/assets/example.ledger /dev/null "${EXAMPLE_FOLDER}/3"
