a5fe23287ccbe5c24f9d95e35b927ac99f460b17
muralisc <muralisc@gmail.com>
2026-06-07 14:17:01 +0100

    Removing as tests will cover

--- example/make_example_graphs.sh
+++
@@ -1,4 +0,0 @@
-EXAMPLE_FOLDER="/var/tmp/ledger/example"
-
-bash ~/src/ledger-graphs/v1/2-monthly-lastmonth-income_expense.sh ~/src/ledger-graphs/assets/example.ledger /dev/null "${EXAMPLE_FOLDER}/2"
-bash ~/src/ledger-graphs/v1/3-monthly-lastmonth-expense_accounts.sh ~/src/ledger-graphs/assets/example.ledger /dev/null "${EXAMPLE_FOLDER}/3"
